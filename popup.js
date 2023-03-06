e111b181c0e11ca7b91961dcfd9479935c00f9bd
IGNITEOZARKS <126728227+IGNITEOZARKS@users.noreply.github.com>
2023-03-06 06:27:49 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -97,7 +97,7 @@ var createDialog = function(text , title) {
             .html('<span style="color: blue;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; [close]</span>');
         $(this).closest('.ui-dialog').css({'width':'430px'});
         $(this).closest('.ui-dialog').css({'position':'absolute',left: (win.width() - $(this).parent().outerWidth()) / 2,top: 168});
-	$(this).closest('.ui-dialog-title').css({'text-align': 'center'});
+	//$(this).closest('.ui-dialog-title').css({'text-align': 'center'});
     }
 })
 
