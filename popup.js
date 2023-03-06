a15ca8763cfb7f57d41425d8c54ac059f22e8173
IGNITEOZARKS <126728227+IGNITEOZARKS@users.noreply.github.com>
2023-03-06 08:33:44 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -93,8 +93,8 @@ var createDialog = function(text , title) {
 	  var win = $(window);  
         $('.ui-dialog-titlebar-close')
             .removeClass("ui-dialog-titlebar-close")
-            .html('<span style="color: blue;">&nbsp; &nbsp; &nbsp; &nbsp;[close]</span>');
-        $(this).closest('.ui-dialog').css({'width':'380px'});
+            .html('<span style="color: blue;">&nbsp; &nbsp; [close]</span>');
+        $(this).closest('.ui-dialog').css({'width':'350px'});
         $(this).closest('.ui-dialog').css({'position':'absolute',left: (win.width() - $(this).parent().outerWidth()) / 2,top: 168});
     }
 })
