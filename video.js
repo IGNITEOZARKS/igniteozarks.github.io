0a1d48f1ce8ea73f77d9b5155774a120ff5b27c5
IGNITEOZARKS <126728227+IGNITEOZARKS@users.noreply.github.com>
2023-04-21 12:52:55 -0500

    Update video.js

--- video.js
+++ video.js
@@ -6,7 +6,5 @@ console.log(g_message);
 var g_date = urlParams.get('dates')
 console.log(g_date);
 
-document.getElementById("vid").innerHTML = 'data-href="https://www.facebook.com/ignitestonecounty/videos/' + g_message + '"';
-document.getElementById("blockvid").innerHTML = '<blockquote cite="https://www.facebook.com/ignitestonecounty/videos/' + g_message + '">';
-document.getElementById("blockfac").innerHTML = '<a href="https://www.facebook.com/ignitestonecounty/videos/' + g_message + '">How to Share With Just Friends</a>';
-document.getElementById("blockdate").innerHTML = g_date;
+var html = '<iframe src="https://www.facebook.com/plugins/video.php?height=311&href=https%3A%2F%2Fwww.facebook.com%2Fignitestonecounty%2Fvideos%2F' + g_message +'%2F&show_text=false&width=560&t=0" width="560" height="311" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>';
+document.getElementById("vid").innerHTML = html;
