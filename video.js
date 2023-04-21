a4da5d84c68544e4d4459317e7321c5cb7c3412c
IGNITEOZARKS <126728227+IGNITEOZARKS@users.noreply.github.com>
2023-04-21 12:28:38 -0500

    Update video.js

--- video.js
+++ video.js
@@ -1,1 +1,5 @@
-
+var queryString = window.location.search;
+console.log(queryString);
+var urlParams = new URLSearchParams(queryString);
+var g_message = urlParams.get('message')
+console.log(g_message);
