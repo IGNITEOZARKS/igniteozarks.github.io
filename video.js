f6bb9546293b0f2d79db8e2734fd032b510dec49
IGNITEOZARKS <126728227+IGNITEOZARKS@users.noreply.github.com>
2023-05-09 08:45:54 -0500

    Update video.js

--- video.js
+++ video.js
@@ -11,7 +11,7 @@ console.log(g_title);
 document.getElementById("title").innerHTML = g_title + ' ~ ' + g_date;
 var html;
 if (g_youtube === "yes"){
-  html = '<iframe id="ytplayer" type="text/html" width="375" height="210.9375"src="' + g_message + '" frameborder="0" allowfullscreen>';
+  html = '<iframe id="ytplayer" type="text/html" width="375" height="210.9375" src="' + g_message + '" frameborder="0" allowfullscreen>';
 }else{
   html = '<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fignitestonecounty%2Fvideos%2F' + g_message +'%2F&width=375&show_text=false&appId=965656367790806&height=208" width="375" height="208" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>';
 
